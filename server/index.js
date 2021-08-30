69945087a7331fdf18e2b0335914885c45b01f57
1smat <reven13022002@gmail.com>
2021-08-30 23:05:28 +0500

    понял

--- server/index.js
+++ server/index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const person = {
+  name: 'ismat',
+  age: 10,
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/users', (req, res) => {
-  res.send('Hello users!')
+  res.send(person)
 })
 
 app.listen(port, () => {
